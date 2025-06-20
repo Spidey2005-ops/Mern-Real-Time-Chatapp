44c57c1ddf8455b399835a0d031f8e0a55f4e46b
Spidey2005-ops <bunnybysani@gmail.com>
2025-06-20 11:39:00 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,33 +5,41 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+// Mapping of userId to socketId
+const userSocketMap = {}; // { userId: socketId }
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173"], // or your frontend deployment URL
+    credentials: true,
   },
 });
 
+// Helper to get a user's socket ID
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+  console.log("A user connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    console.log(`Mapped userId ${userId} to socket ${socket.id}`);
+  }
+
+  // Notify all clients about currently online users
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    if (userId) {
+      delete userSocketMap[userId];
+      console.log(`User ${userId} disconnected and removed from socket map`);
+    }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
-export { io, app, server };
+export { app, server, io };
